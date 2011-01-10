4f334f91f0ad4869aad54fb07f8c1bcb39606022
WayneGerard <WayneGerard@afbfa1bf-a89b-8cfb-9741-8ef9c2038bb6>
2011-01-10 00:55:34 +0000

    it compiles, omg

--- trunk/proj/Server.java
+++ trunk/proj/Server.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.StringTokenizer;
 
 import edu.washington.cs.cse490h.lib.Node;
@@ -69,8 +71,115 @@ public class Server extends RIONode {
 	
 	}
 	
-	public void createFile(){
+	/**
+	 * Creates a file on the local filesystem
+	 * @param fileName the file to create
+	 */
+	public void createFile(String fileName){
+		fileName = this.dataDirectory + fileName;
+		File filePath = new File(fileName);
+		// check if the file exists
+		if (filePath.exists()){
+			printError(11);
+		}
 		
+		// create the file
+		else{
+			try{
+				PersistentStorageWriter writer = getWriter(fileName, false);
+				writer.close();
+			} catch (IOException e)
+			{
+				System.err.println(e.getMessage());
+				System.err.println(e.getStackTrace());
+			}
+		}
+	}
+	
+	/**
+	 * Deletes a file from the local file system. Fails and prints an error if the file does not exist
+	 * @param fileName the file name to delete
+	 */
+	public void deleteFile(String fileName){
+		fileName = this.dataDirectory + fileName;
+		// check if the file even exists
+		File filePath = new File(fileName);
+		if (!filePath.exists())
+			printError(10);
+		else{
+			// delete file
+			try{
+				PersistentStorageWriter writer = getWriter(fileName, false);
+				writer.delete();
+				writer.close();
+			} catch (IOException e) 
+			{
+				System.err.println(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Sends a file to the client with the given filename
+	 * @param fileName the filename to send
+	 */
+	public void getFile(String fileName, int from){
+		
+		fileName = this.dataDirectory + fileName;
+		// check if the file exists
+		File filePath = new File(fileName);
+		if (!filePath.exists())
+			printError(10);
+		// send the file if it does
+		else{
+			// load the file into a reader
+			String sendMsg = "";
+			String inLine = "";
+			try{
+				PersistentStorageReader reader = getReader(fileName);
+				while (!((inLine = reader.readLine()) == null))
+					sendMsg += inLine;
+				reader.close();
+			} catch (FileNotFoundException e){
+				// file not found, but should have been caught earlier...
+			} catch (IOException e){
+				// ioexception
+			}
+			
+			// send the payload
+			byte[] payload = Utility.stringToByteArray(sendMsg);
+			RIOLayer.RIOSend(from, Protocol.DATA, payload);
+		}
+	}
+	
+	/**
+	 * Writes a file to the local filesystem. Fails if the file does not exist already
+	 * @param filename the file name to write to
+	 * @param contents the contents to write
+	 */
+	public void writeFile(String fileName, String contents, int protocol)
+	{
+		fileName = "./Data/" + fileName;
+		// check if the file exists
+		File filePath = new File(fileName);
+		if (!filePath.exists())
+			printError(10);
+		else{
+			try{
+				PersistentStorageWriter writer = null;
+				// create a new file writer, setting the append option appropriately
+				if (protocol == Protocol.APPEND){
+					writer = getWriter(fileName, true);
+				} else {
+					writer = getWriter(fileName, false);
+				}
+				writer.write(contents);
+				writer.close();
+			} catch (IOException e){
+				// ioexception
+			}
+		}
 	}
 	
 	/**
@@ -86,51 +195,13 @@ public class Server extends RIONode {
 	public void onRIOReceive(Integer from, int protocol, byte[] msg)
 	{
 		String msgString = Utility.byteArrayToString(msg);
-		//TODO: Break out into different methods
-		if (protocol == Protocol.CREATE) {
-			File filePath = new File("./Data/" + msgString);
-			// check if the file exists
-			if (filePath.exists()){
-				printError(11);
-			}
-			// create the file
-			else{
-				PersistentStorageWriter writer = getWriter("./Data/" + msgString, false);
-				writer.close();
-			}
 		
+		if (protocol == Protocol.CREATE) {
+			createFile(msgString);
 		}else if (protocol == Protocol.DELETE){
-			// check if the file even exists
-			File filePath = new File("./Data/" + msgString);
-			if (!filePath.exists())
-				printError(10);
-			else{
-				// delete file
-				PersistentStorageWriter writer = getWriter("./Data/" + msgString, false);
-				writer.delete();
-				writer.close();
-			}
-			
+			deleteFile(msgString);
 		}else if (protocol == Protocol.GET){
-			// check if the file exists
-			File filePath = new File("./Data/" + msgString);
-			if (!filePath.exists())
-				printError(10);
-			// send the file if it does
-			else{
-				// load the file into a reader
-				String sendMsg = "";
-				String inLine = "";
-				PersistentStorageReader reader = getReader("./Data/" + msgString);
-				while (!((inLine = reader.readLine()) == null))
-						sendMsg += inLine;
-				reader.close();
-				
-				// send the payload
-				byte[] payload = Utility.stringToByteArray(sendMsg);
-				RIOLayer.RIOSend(from, Protocol.DATA, payload);
-			}
-			
+			getFile(msgString, from);
 		}else if (protocol == Protocol.PUT || protocol == Protocol.APPEND){
 
 			// tokenize the string and parse out the contents and filename
@@ -139,21 +210,7 @@ public class Server extends RIONode {
 			int length = fileName.length();
 			String contents = msgString.substring(length+1, msgString.length());
 			
-			// check if the file exists
-			File filePath = new File("./Data/" + fileName);
-			if (!filePath.exists())
-				printError(10);
-			else{
-				PersistentStorageWriter writer = null;
-				// create a new file writer, setting the append option appropriately
-				if (protocol == Protocol.APPEND){
-					writer = getWriter("./Data/" + msgString, true);
-				} else {
-					writer = getWriter("./Data/" + msgString, false);
-				}
-				writer.write(contents);
-				writer.close();
-			}
+			writeFile(fileName, contents, protocol);
 		}
 		
 		
