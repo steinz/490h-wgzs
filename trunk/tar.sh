c319bd82f1bfd6e42ca947f9740688ce533546d8
z.b.stein <z.b.stein@afbfa1bf-a89b-8cfb-9741-8ef9c2038bb6>
2011-01-14 23:56:28 +0000

    tarballs and script

---
+++ trunk/tar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+tar -cvzf cse490h-wgzs.tar cse490h-wgzs/proj/*.java cse490h-wgzs/scripts/FSTest* cse490h-wgzs/scripts/Handshake* cse490h-wgzs/writeups/project*/writeup.pdf cse490h-wgzs/writeups/project*/writeup.tex
