3e52986b8861edbe3a5e2622c0ff848a1a2e79b2
z.b.stein <z.b.stein@afbfa1bf-a89b-8cfb-9741-8ef9c2038bb6>
2011-01-24 05:02:02 +0000

    onCommand cache coherence implementations
    WD implementation

--- trunk/proj/PacketPackException.java
+++ trunk/proj/PacketPackException.java
@@ -1,3 +1,4 @@
+
 public class PacketPackException extends Exception {
 
 	private static final long serialVersionUID = 5273893683486775453L;
---
+++ trunk/proj/UnknownManagerException.java
@@ -0,0 +1,6 @@
+
+public class UnknownManagerException extends Exception {
+
+	private static final long serialVersionUID = 6525390876463186997L;
+
+}
