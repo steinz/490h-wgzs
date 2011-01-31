164ee486d498108f9190a2f1f45cf9e47efd58a8
z.b.stein <z.b.stein@afbfa1bf-a89b-8cfb-9741-8ef9c2038bb6>
2011-01-31 03:40:18 +0000

    Probable bug in managerSendFile. Commiting before fixing, just in case...

--- trunk/proj/NotManagerException.java
+++ trunk/proj/NotManagerException.java
@@ -5,6 +5,14 @@
 
 public class NotManagerException extends Exception {
 
+	public NotManagerException(String string) {
+		super(string);
+	}
+
+	public NotManagerException() {
+		super();
+	}
+
 	private static final long serialVersionUID = -2133442099641600446L;
 
 }
