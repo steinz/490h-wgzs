497c3729cb72f381868308348538ddcf68b6dca8
z.b.stein <z.b.stein@afbfa1bf-a89b-8cfb-9741-8ef9c2038bb6>
2011-03-04 02:21:34 +0000

    Improved LogFS interface.

--- trunk/proj/edu/washington/cs/cse490h/tdfs/LogFS.java
+++ trunk/proj/edu/washington/cs/cse490h/tdfs/LogFS.java
@@ -1,68 +1,28 @@
 package edu.washington.cs.cse490h.tdfs;
 
-import java.util.List;
-
 public interface LogFS {
-	
-	//TODO: HIGH: Replace??? w/:
-	
-	public void writeLogEntry(Operation op);
-	
-	public Operation getLogEntry();
-	
-	public int nextLogNumber();
-	
-	
 
-	public Integer checkLocked(String filename)
-			throws NotParticipatingException;
-
-	public void createFile(String filename, int address)
-			throws FileAlreadyExistsException, NotParticipatingException,
-			FileLockedByAnotherAddressException;
+	public Integer checkLocked(String filename) throws NotListeningException;
 
 	public void createGroup(String filename)
 			throws AlreadyParticipatingException;
 
-	public void deleteFile(String filename, int address)
-			throws FileDoesNotExistException, NotParticipatingException,
-			FileLockedByAnotherAddressException;
+	public boolean fileExists(String filename) throws NotListeningException;
 
-	public boolean fileExists(String filename) throws NotParticipatingException;
+	public String getFile(String filename) throws NotListeningException;
 
-	public String getFile(String filename) throws FileDoesNotExistException,
-			NotParticipatingException;
+	public LogEntry getLogEntry(String filename, int operationNumber)
+			throws NotListeningException, NoSuchOperationNumberException;
 
-	public int getNextOperationNumber(String filename)
-			throws NotParticipatingException;
+	public boolean isListening(String filename);
 
-	public Operation getOperation(String filename, int operationNumber)
-			throws NotParticipatingException, NoSuchOperationNumberException;
-
-	public List<Integer> getParticipants(String filename)
-			throws NotParticipatingException;
-
-	public boolean isParticipating(String filename);
-
-	public void join(String filename, int address)
-			throws NotParticipatingException;
-
-	public void joinGroup(String filename, byte[] packedLog)
+	public void listen(String filename, byte[] packedLog)
 			throws AlreadyParticipatingException;
 
-	public void leave(String filename, int address)
-			throws NotParticipatingException;
+	public int nextLogNumber(String filename) throws NotListeningException;
 
-	public void lockFile(String filename, int address)
-			throws NotParticipatingException, AlreadyLockedException;
+	public byte[] packLog(String filename) throws NotListeningException;
 
-	public byte[] packLog(String filename) throws NotParticipatingException;
+	public void writeLogEntry(LogEntry op) throws NotListeningException;
 
-	public void unlockFile(String filename, int address)
-			throws NotParticipatingException, NotLockedException,
-			FileLockedByAnotherAddressException;
-
-	public void writeFile(String filename, String content, boolean append,
-			int address) throws FileDoesNotExistException,
-			NotParticipatingException, FileLockedByAnotherAddressException;
 }
