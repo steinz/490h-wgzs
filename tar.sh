eeb2ffbb2dc467cdde2d378bff11c5aea3acc887
z.b.stein <z.b.stein@afbfa1bf-a89b-8cfb-9741-8ef9c2038bb6>
2011-03-08 04:35:21 +0000

    updated tar script, removed old packages/tests

--- tar.sh
+++ tar.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 OUTPUT_FILE="cse490h-wgzs.tar.gz"
-SOURCE="cse490h-wgzs/proj/*.java cse490h-wgzs/tests/*.java"
+SOURCE="cse490h-wgzs/proj/edu/washington/cs/ces490h/tdfs/*.java cse490h-wgzs/tests/*.java"
+TEST="cse490h-wgzs/tests/edu/washington/cs/cse490h/tdfs/tests/*.java"
 LIB="cse490h-wgzs/jars/*.jar cse490h-wgzs/jars/lib/*.jar"
 SCRIPTS="cse490h-wgzs/*.sh cse490h-wgzs/*.pl cse490h-wgzs/*.py"
-SIM_SCRIPTS="cse490h-wgzs/simulator_scripts/Cache* cse490h-wgzs/simulator_scripts/test*"
 WRITEUPS="cse490h-wgzs/writeups/project*/*.pdf cse490h-wgzs/writeups/project*/*.png"
-OUTPUT=""
-#OUTPUT="cse490h-wgzs/output/*/*.png"
-SYNOPTIC="cse490h-wgzs/synoptic_args/*.args"
 
-tar -cvzf $OUTPUT_FILE $SOURCE $LIB $SCRIPTS $SIM_SCRIPTS $WRITEUPS $OUTPUT $SYNOPTIC
+tar -cvzf $OUTPUT_FILE $SOURCE $LIB $SCRIPTS $WRITEUPS 
