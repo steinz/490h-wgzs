78b3028f456b55084405da10fc4bdd48a2ab8731
z.b.stein <z.b.stein@afbfa1bf-a89b-8cfb-9741-8ef9c2038bb6>
2011-03-11 14:07:58 +0000

    Another toy CommandDAG implementation.

---
+++ trunk/proj/edu/washington/cs/cse490h/tdfs/CommandDAG.java
@@ -0,0 +1,125 @@
+package edu.washington.cs.cse490h.tdfs;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A DAG of dependent commands to execute. A factory of CommandNodes.
+ */
+public class CommandDAG<K> {
+	/**
+	 * Encapsulates a function to be run by a CommandNode
+	 */
+	public static abstract class Command<AK, AV> {
+		public abstract void execute(Map<AK, AV> args);
+	}
+
+	/**
+	 * Encapsulates a single command the user wants to execute.
+	 */
+	public abstract class CommandNode<AK, AV, RK, RV> {
+
+		/**
+		 * A list of CommandNodes dependent on the successful execution of this
+		 * CommandNode.
+		 */
+		private List<CommandNode<RK, RV, ?, ?>> children;
+		/**
+		 * The function that this CommandNode executes.
+		 */
+		private Command<AK, AV> command;
+		/**
+		 * The function that this CommandNode executes on failure.
+		 */
+		private Command<AK, AV> failCommand;
+		/**
+		 * A key for this CommandNode unique amongst executing Commands
+		 */
+		private K key;
+		/**
+		 * The number of parents this CommandNode has.
+		 */
+		private int locks;
+		/**
+		 * Args for this command.
+		 */
+		private Map<AK, AV> args;
+
+		/**
+		 * Creates a dangling CommandNode in the graph with the given commands.
+		 */
+		public CommandNode(Command<AK, AV> command,
+				Command<AK, AV> failCommand, K key) {
+			this.command = command;
+			this.failCommand = failCommand;
+			this.key = key;
+			this.locks = 0;
+
+			dangling.put(key, this);
+		}
+
+		/**
+		 * Notify children of results.
+		 */
+		private void done(Map<RK, RV> results) {
+			executing.remove(this);
+			for (CommandNode<RK, RV, ?, ?> child : children) {
+				child.parentFinished(results);
+			}
+		}
+
+		/**
+		 * Execute this Command.
+		 */
+		private void execute() {
+			executing.put(key, this);
+			command.execute(this.args);
+		}
+
+		/**
+		 * Fail this command.
+		 */
+		private void fail() {
+			executing.remove(key);
+			failCommand.execute(this.args);
+		}
+
+		/**
+		 * Adds parent's results to args and executes if unlocked.
+		 */
+		private void parentFinished(Map<AK, AV> args) {
+			this.args.putAll(args);
+			this.locks--;
+			if (this.locks == 0) {
+				execute();
+			}
+		}
+	}
+
+	private Map<K, CommandNode<?, ?, ?, ?>> dangling;
+	private Map<K, CommandNode<?, ?, ?, ?>> executing;
+
+	public CommandDAG() {
+		this.dangling = new HashMap<K, CommandNode<?, ?, ?, ?>>();
+		this.executing = new HashMap<K, CommandNode<?, ?, ?, ?>>();
+	}
+
+//	// TODO: cont?
+//	public void cont(K key, Map<?, ?> results) {
+//		CommandNode n = executing.get(key);
+//		n.cont(results);
+//	}
+
+	public void done(K key) {
+		executing.get(key).done(null);
+	}
+
+	public void exec(K key) {
+		dangling.get(key).execute();
+	}
+
+	public void fail(K key) {
+		executing.get(key).fail();
+	}
+}
